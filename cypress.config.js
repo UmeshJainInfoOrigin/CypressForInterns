3189aa1546774a01ee78e656400fbd9621fe1289
Umesh Jani <umesh.jain@infoorigin.com>
2023-07-11 15:51:11 +0530

    Connecting to mySQL and command

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,52 @@
 const { defineConfig } = require("cypress");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+const mysql = require("mysql2");
+
+function queryTestDb(query, config) {
+  // creates a new mysql connection using credentials from cypress.json env's
+  const connection = mysql.createConnection(config.env.db);
+  // start connection to db
+  connection.connect();
+  // exec query + disconnect to db as a Promise
+  return new Promise((resolve, reject) => {
+    connection.query(query, (error, results) => {
+      if (error) reject(error);
+      else {
+        connection.end();
+        console.log(results)
+        return resolve(results);
+      }
+    });
+  });
+}
 
 async function setupNodeEvents(on, config) {
   // implement node event listeners here
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
   on("file:preprocessor", browserify.default(config));
+  
+  on("task", {
+    queryDb: (query) => {
+      return queryTestDb(query, config);
+    }
+  }); 
+  
   return config;
 }
 
 module.exports = defineConfig({
+  env: {
+    "db": {
+      "host": "127.0.0.1",
+      "user": "root",
+      "password": "MyNewPass",
+      "database": "test",
+      "port": 3306,
+      //"k  ey": "cypress/pemFiles/testPemFile.pem",
+      //"http": true
+    },
+  },
   e2e: {
     setupNodeEvents,
     specPattern: [
--- cypress/e2e/Login/loginStepDef.js
+++ cypress/e2e/Login/loginStepDef.js
@@ -1,5 +1,33 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then, afterScreenshotHandler } from "@badeball/cypress-cucumber-preprocessor";
 
   Given("Actor visit site", ()=> {
     cy.visit('https://example.cypress.io')
+    
+   
+    cy.connectToMySql('SELECT * FROM application').then(count => {
+        expect(count).to.have.lengthOf(8);
+      });
+      cy.log('after my command')
+    
+    cy.task(
+        "queryDb",
+        `SELECT * FROM application`
+      ).then(count => {
+        expect(count).to.have.lengthOf(8);
+      });
+
+      cy.task('queryDb', `delete from client WHERE ClientID in ("C4", "C5")`).then((result) => {
+        expect(result.changedRows).to.greaterThan(-1)
+    })
+
+      cy.task('queryDb', `INSERT INTO client (ClientID, ClientName, Description, CreatedBy) VALUES
+      ("C4", "John", "QA Lead", "InfoOrigin"),
+      ("C5", "Smith", "Product Owner", "InfoOrigin");`).then((result) => {
+              expect(result.affectedRows).to.equal(2)
+    })
+
+    cy.task('queryDb', `UPDATE client SET ClientName = "Smith" WHERE ClientID="C1"`).then((result) => {
+        expect(result.changedRows).to.equal(1)
+    })
+
   })
